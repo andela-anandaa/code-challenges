278e3119058bd932867f530e00f125d96f83e01c
Anthony Nandaa <profnandaa@gmail.com>
2016-11-11 01:25:39 +0300

    clean-up iq_test

--- codewars/iq_test/iq_test.py
+++ codewars/iq_test/iq_test.py
@@ -1,21 +1,14 @@
-'''
-@source: https://www.codewars.com/kata/552c028c030765286c00007d
-
-Bob is preparing to pass IQ test. The most frequent task in this test is **to find out which one of the given numbers differs from the others**. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
-
-> ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from **1 (not 0)**.
-'''
-
 def iq_test(numbers):
-    nums = map(int, numbers.split(' '))
-    same_count = 0
-
-    for i in range(1, len(nums)):
-        if nums[i] == nums[i - 1]:
-            same_count += 1
-        elif same_count > 1:
-            return i + 1
-    return -1
-
-        
-
+    nums = map(int, numbers.split(" "))
+    evens, odds = 0, 0
+    for i, n  in enumerate(nums):
+        if n % 2 == 0:
+            evens += 1
+            ei = i
+        else:
+            odds += 1
+            oi = i
+    
+    if evens == 1:
+        return ei + 1
+    return oi + 1
--- codewars/iq_test/test_iq_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from iq_test.iq_test import iq_test
-
-class IQTestTestCase(unittest.TestCase):
-
-    def test_cases(self):
-        self.assertEquals(iq_test("2 4 7 8 10"), 3)
-        self.assertEquals(iq_test("1 2 2"), 1)
